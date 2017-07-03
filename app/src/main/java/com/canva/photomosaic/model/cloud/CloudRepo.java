2d010a709bc8dce0cc5e30cb44fb95fb566754c9
Eslam Hussein <eng.eslam.hussein1@gmail.com>
2017-07-03 20:12:42 +0200

    add time out to httprequest

--- app/src/main/java/com/canva/photomosaic/model/cloud/CloudRepo.java
+++ app/src/main/java/com/canva/photomosaic/model/cloud/CloudRepo.java
@@ -3,7 +3,6 @@ package com.canva.photomosaic.model.cloud;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 
-import com.canva.photomosaic.business.BitmapAverageCalculator;
 import com.canva.photomosaic.model.dto.Tile;
 
 import java.io.IOException;
@@ -14,14 +13,14 @@ import java.net.URL;
 
 public class CloudRepo {
 
+    private static final String BASE_URL = "http://192.168.1.9:8765/color/";
 
     public Bitmap getImage(Tile tile) throws IOException {
-
-
         URL url = new URL(buildUrl(tile));
         HttpURLConnection connection = (HttpURLConnection) url
                 .openConnection();
         connection.setDoInput(true);
+        connection.setConnectTimeout(1000);
         connection.connect();
         InputStream input = connection.getInputStream();
         Bitmap myBitmap = BitmapFactory.decodeStream(input);
@@ -30,10 +29,7 @@ public class CloudRepo {
 
 
     private String buildUrl(Tile tile) {
-        return "http://192.168.1.9:8765/color/".
-                concat(String.valueOf(tile.getWidth()).concat("/").
-                        concat(String.valueOf(tile.getHeight())).concat("/").
-                        concat(tile.getAvgColor()));
-
+        return BASE_URL.concat(String.valueOf(tile.getWidth()).concat("/").
+                concat(String.valueOf(tile.getHeight())).concat("/").concat(tile.getAvgColor()));
     }
 }
